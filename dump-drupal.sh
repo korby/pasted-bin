c4c4aab10f4ae1f40b92d8e6e64742a9f2ae73de
André Cianfarani <acianfa@gmail.com>
2019-05-09 16:23:14 +0200

    dump

--- dump-drupal.sh
+++ dump-drupal.sh
@@ -5,4 +5,4 @@ db=$(grep database $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "
 usr=$(grep username $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 pass=$(grep password $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 echo $pwd
-echo mysqldump -h $host -u $usr -p $db > db-$(date '+%Y-%m-%d_%H-%M-%S').sql 
+echo "mysqldump -h $host -u $usr -p $db > db-$(date '+%Y-%m-%d_%H-%M-%S').sql "
