6675929426b0bc883295cccc19223a5b3546f306
André Cianfarani <acianfa@gmail.com>
2019-05-13 10:06:15 +0200

    Dumps improve

--- dump-drupal.sh
+++ dump-drupal.sh
@@ -5,4 +5,4 @@ db=$(egrep "('|\")database('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#
 usr=$(egrep "('|\")username('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 pass=$(egrep "('|\")password('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 echo "password: "$pass
-echo "mysqldump -h $host -u $usr -p $db > $host-$(date '+%Y-%m-%d_%H-%M-%S').sql"
+echo "mysqldump -h $host -u $usr -p $db > $db-$(date '+%Y-%m-%d_%H-%M-%S').sql"
--- dump-wordpress.sh
+++ dump-wordpress.sh
@@ -5,4 +5,4 @@ db=$(egrep "^[^/].*('|\")DB_NAME('|\")(,)+" $settings| grep -v "^ \*"  | grep -v
 usr=$(egrep "^[^/].*('|\")DB_USER('|\")(,)+" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 pass=$(egrep "^[^/].*('|\")DB_PASSWORD('|\")(,)+" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 echo "password: "$pass
-echo "mysqldump -h $host -u $usr -p $db > $host-$(date '+%Y-%m-%d_%H-%M-%S').sql"
+echo "mysqldump -h $host -u $usr -p $db > $db-$(date '+%Y-%m-%d_%H-%M-%S').sql"
