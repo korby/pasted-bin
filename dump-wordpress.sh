326dd24f29efab67c3914e2fe0bd62a71dc18201
André Cianfarani <acianfa@gmail.com>
2019-05-13 10:01:14 +0200

    Worpress dump improve

--- dump-wordpress.sh
+++ dump-wordpress.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 settings=./wp-config.php
-host=$(egrep "('|\")DB_HOST('|\")(,)+" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
-db=$(egrep "('|\")DB_NAME('|\")(,)+" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
-usr=$(egrep "('|\")DB_USER('|\")(,)+" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
-pass=$(egrep "('|\")DB_PASSWORD('|\")(,)+" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
+host=$(egrep "^[^/].*('|\")DB_HOST('|\")(,)+" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
+db=$(egrep "^[^/].*('|\")DB_NAME('|\")(,)+" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
+usr=$(egrep "^[^/].*('|\")DB_USER('|\")(,)+" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
+pass=$(egrep "^[^/].*('|\")DB_PASSWORD('|\")(,)+" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 echo "password: "$pass
 echo "mysqldump -h $host -u $usr -p $db > db-$(date '+%Y-%m-%d_%H-%M-%S').sql"
