61c25cfd435bc8d506e83f3e365281605fbddca7
André Cianfarani <andre.cianfarani@insign.fr>
2019-05-21 09:30:05 +0200

    Fix check severeal paths

--- dump-drupal.sh
+++ dump-drupal.sh
@@ -2,9 +2,9 @@
 path_1="current/web/sites/default/settings.env.php"
 path_2="web/sites/default/settings.env.php"
 path_3="sites/default/settings.env.php"
-if [ -f "$path_1" ]; settings=$path_1; fi
-if [ -f "$path_2" ]; settings=$path_2; fi
-if [ -f "$path_3" ]; settings=$path_3; fi
+if [ -f "$path_1" ]; then settings=$path_1; fi
+if [ -f "$path_2" ]; then settings=$path_2; fi
+if [ -f "$path_3" ]; then settings=$path_3; fi
 host=$(egrep "('|\")host('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 db=$(egrep "('|\")database('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 usr=$(egrep "('|\")username('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
