f8fdc0f0f2d5960aa3a6b4530532e010e0876dc3
André Cianfarani <andre.cianfarani@insign.fr>
2019-06-11 11:48:59 +0200

    Fix magento

--- dump-magento.sh
+++ dump-magento.sh
@@ -6,7 +6,7 @@ if [ -f "$path_1" ]; then settings=$path_1; fi
 if [ -f "$path_2" ]; then settings=$path_2; fi
 if [ -f "$path_3" ]; then settings=$path_3; fi
 host=$(egrep "('|\")host('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
-db=$(egrep "('|\")database('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
+db=$(egrep "('|\")dbname('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 usr=$(egrep "('|\")username('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 pass=$(egrep "('|\")password('|\")( )+=>" $settings| grep -v "^ \*"  | grep -v "^#"| cut -d "$(printf "\x27")" -f 4)
 echo "password: "$pass
