239a428f0558c44e137e2844fb1b289223405f72
André Cianfarani <andre.cianfarani@insign.fr>
2019-09-20 10:07:21 +0200

    add wp-cli

--- wp-cli-install.sh
+++ wp-cli-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-echo "alias wp-cli='$(pwd)/wp-cli.phar'" ~/.bashrc
+echo "alias wp-cli='$(pwd)/wp-cli.phar'" >> ~/.bashrc
 . ~/.bashrc
