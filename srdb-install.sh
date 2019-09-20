8cc1f01459ef4e73cfeae6a5e4168cb10c3ec208
André Cianfarani <andre.cianfarani@insign.fr>
2019-09-20 09:57:38 +0200

    Improve srdb

--- srdb-install.sh
+++ srdb-install.sh
@@ -6,3 +6,5 @@ mkdir srdb
 cd srdb
 wget https://raw.githubusercontent.com/interconnectit/Search-Replace-DB/master/srdb.class.php
 wget https://raw.githubusercontent.com/interconnectit/Search-Replace-DB/master/srdb.cli.php
+sed -i "s/<?php/<?php\nerror_reporting(E_ERROR);/" srdb.cli.php
+chmod +x srdb.cli.php
